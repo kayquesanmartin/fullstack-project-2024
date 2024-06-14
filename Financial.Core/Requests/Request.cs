62cf486b268f0440da7d92377b2096d91eb8ff99
kayquesanmartin <kayque.sanmartin65@gmail.com>
2024-06-14 19:01:36 -0300

    Finalizado o dia 1

---
+++ Financial.Core/Requests/Request.cs
@@ -0,0 +1,7 @@
+namespace Financial.Core.Requests;
+
+public abstract class Request
+{
+    public string UserId { get; set; } = string.Empty;
+    
+}
---
+++ Financial.Core/Requests/Transactions/DeleteTransactionRequest.cs
@@ -0,0 +1,6 @@
+namespace Financial.Core.Requests.Transactions;
+
+public class DeleteTransactionRequest : Request
+{
+    public long Id { get; set; }
+}
